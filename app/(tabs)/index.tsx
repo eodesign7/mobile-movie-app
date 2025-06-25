0ed6d487bbfe963f54c679429bc3975cf726eb01
Erik Ondruš <ondrus.eodesign@gmail.com>
2025-06-25 11:14:48 +0200

    ✅Home Screen: SearchBar, SearchFunctionality, CustomHook(fetchMovies)

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,55 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import { useRouter } from "expo-router";
+import { ActivityIndicator, Image, ScrollView, Text, View } from "react-native";
 
-export default function Index() {
+export default function Home() {
+  const router = useRouter();
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() => fetchMovies({ query: "" }));
+
+  const handleSearch = () => {
+    router.push("/search");
+  };
+
   return (
-    <View className="flex-1 gap-2 items-center justify-center">
-      <Text className="text-5xl font-bold text-center text-primary">
-        Welcome to the Movie App
-      </Text>
-      <Text className="text-2xl font-light text-center text-secondary">
-        Discover the latest movies and TV shows
-      </Text>
-      
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0" />
+
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          minHeight: "100%",
+          paddingBottom: 10,
+        }}
+      >
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+
+        {moviesLoading ? (
+          <ActivityIndicator
+            size="large"
+            color="#0000ff"
+            className="mt-10 self-center"
+          />
+        ) : moviesError ? (
+          <Text className="text-white text-center">{moviesError?.message}</Text>
+        ) : (
+          <View className="flex-1 mt-5">
+            <SearchBar
+              onPress={handleSearch}
+              placeholder="Search for a movie"
+            />
+          </View>
+        )}
+      </ScrollView>
     </View>
   );
 }
